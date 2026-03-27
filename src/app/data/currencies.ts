9bb29b33b4243a9acec45b9b5be4749dde0fc9cc
Henrique Simon <henriquesimon1998@gmail.com>
2026-03-27 20:37:40 -0300

    feat: add currency selection with custom dropdown

---
+++ src/app/data/currencies.ts
@@ -0,0 +1,13 @@
+export const CURRENCIES = [
+    { code: 'USD', name: 'Dólar Americano', symbol: '$', flag: 'https://flagcdn.com/us.svg' },
+    { code: 'EUR', name: 'Euro', symbol: '€', flag: 'https://flagcdn.com/eu.svg' },
+    { code: 'BRL', name: 'Real Brasileiro', symbol: 'R$', flag: 'https://flagcdn.com/br.svg' },
+    { code: 'GBP', name: 'Libra Esterlina', symbol: '£', flag: 'https://flagcdn.com/gb.svg' },
+    { code: 'JPY', name: 'Iene Japonês', symbol: '¥', flag: 'https://flagcdn.com/jp.svg' },
+    { code: 'CAD', name: 'Dólar Canadense', symbol: 'C$', flag: 'https://flagcdn.com/ca.svg' },
+    { code: 'AUD', name: 'Dólar Australiano', symbol: 'A$', flag: 'https://flagcdn.com/au.svg' },
+    { code: 'CHF', name: 'Franco Suíço', symbol: 'CHF', flag: 'https://flagcdn.com/ch.svg' },
+    { code: 'CNY', name: 'Yuan Chinês', symbol: '¥', flag: 'https://flagcdn.com/cn.svg' },
+    { code: 'ARS', name: 'Peso Argentino', symbol: '$', flag: 'https://flagcdn.com/ar.svg' },
+    { code: 'MXN', name: 'Peso Mexicano', symbol: '$', flag: 'https://flagcdn.com/mx.svg' }
+];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CURRENCIES } from '../../data/currencies';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
 
+export class Home {
+  currencies = CURRENCIES;
+  form: FormGroup;
+
+  isOpenFrom = false;
+  isOpenTo = false;
+
+  selectedFrom = this.currencies[0];
+  selectedTo = this.currencies[2]; // BRL por exemplo
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      valor: [''],
+      from: ['USD'],
+      to: ['BRL']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+
+  // FROM
+  toggleFrom() {
+    this.isOpenFrom = !this.isOpenFrom;
+  }
+
+  selectFrom(currency: any, event: Event) {
+    event.stopPropagation();
+    this.selectedFrom = currency;
+    this.form.patchValue({ from: currency.code }); // 🔥 integra com form
+    this.isOpenFrom = false;
+  }
+
+  // TO
+  toggleTo() {
+    this.isOpenTo = !this.isOpenTo;
+  }
+
+  selectTo(currency: any, event: Event) {
+    event.stopPropagation();
+    this.selectedTo = currency;
+    this.form.patchValue({ to: currency.code }); // 🔥 integra com form
+    this.isOpenTo = false;
+  }
 }
